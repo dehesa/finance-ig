0a977359c61b043e3023a69ad53fc5f3de399cee
Marcos Sánchez-Dehesa Carballo <san.dehesa@gmail.com>
2020-07-28 11:33:06 +0200

    Prepare release 0.10.0

--- IG/sources/database/DB.swift
+++ IG/sources/database/DB.swift
@@ -31,7 +31,7 @@ public final class Database {
     /// - parameter location: The location of the database (whether "in-memory" or file system).
     /// - parameter targetQueue: The target queue on which to process the `Database` requests and responses.
     /// - throws: `IG.Error` exclusively.
-    public convenience init(location: Database.Location, targetQueue: DispatchQueue?) throws {
+    public convenience init(location: Database.Location, targetQueue: DispatchQueue? = nil) throws {
         let queue = targetQueue ?? DispatchQueue(label: Bundle.IG.identifier + ".database.queue", qos: .utility, attributes: .concurrent, autoreleaseFrequency: .inherit, target: targetQueue)
         let channel = try Database.Channel(location: location, targetQueue: targetQueue)
         try self.init(channel: channel, queue: queue)
